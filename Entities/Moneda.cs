f2d860f80307fabb0174e5e2396784c6c0688740
FrancoScaglione203 <franco.scaglione2@gmail.com>
2023-10-18 00:37:24 -0300

    Creacion entities

---
+++ Entities/Moneda.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TrabajoFinalSofttek.Entities
+{
+    public class Moneda
+    {
+        [Column("moneda_id")]
+        public int Id { get; set; }
+
+        [Column("moneda_descripcion")]
+        public string Descripcion { get; set; }
+    }
+}
