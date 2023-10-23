dd67f20c8d61a8aa84e5f842dae92469a1e4e614
FrancoScaglione203 <franco.scaglione2@gmail.com>
2023-10-22 21:03:40 -0300

    EndPoints DepositoByCuil, ExtraccionByCuil y GetByCuil de los controllers de CuentaFiduciaria y CuentaCripto

--- DataAccess/Repositories/CuentaCriptoRepository.cs
+++ DataAccess/Repositories/CuentaCriptoRepository.cs
@@ -1,4 +1,5 @@
-﻿using TrabajoFinalSofttek.DataAccess.Repositories.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using TrabajoFinalSofttek.DataAccess.Repositories.Interfaces;
 using TrabajoFinalSofttek.Entities;
 
 namespace TrabajoFinalSofttek.DataAccess.Repositories
@@ -7,6 +8,40 @@ namespace TrabajoFinalSofttek.DataAccess.Repositories
     {
         public CuentaCriptoRepository(ApplicationDbContext context) : base(context)
         {
+        }
+
+        public async Task<CuentaCripto> GetByCuil(long cuil)
+        {
+            var usuario = await _context.Usuarios.SingleOrDefaultAsync(u => u.Cuil == cuil);
+            var cuentaCripto = await _context.CuentasCriptos.SingleOrDefaultAsync(u => u.Id == usuario.CuentaCriptoId);
+            return cuentaCripto;
+        }
+
+
+        public async Task<bool> DepositoByCuil(long cuil, decimal monto)
+        {
+            var cuentaCripto = await GetByCuil(cuil);
+            if (cuentaCripto == null || monto <= 0) { return false; }
+
+                cuentaCripto.Saldo += monto;
+            
+            _context.CuentasCriptos.Update(cuentaCripto);
+            return true;
+        }
+
+
+        public async Task<bool> ExtraccionByCuil(long cuil, decimal monto)
+        {
+            var cuentaCripto = await GetByCuil(cuil);
+            if (cuentaCripto == null || monto <= 0) { return false; }
+
+            if (monto <= cuentaCripto.Saldo)
+            {
+                cuentaCripto.Saldo -= monto;
+                _context.CuentasCriptos.Update(cuentaCripto);
+                return true;
+            }
+            else { return false; }
         }
     }
 }
